87304c418160a8bde225e21975a83056fcd46850
vanshkamboj <vanshkamboj.mca@gmail.com>
2020-01-22 10:08:12 +0530

    Add new method

--- programm1_pattern.c
+++ programm1_pattern.c
@@ -42,3 +42,54 @@ int main()
 
     return 0;
 }
+
+
+//different mathod with two loops only
+
+#include <stdio.h>
+
+void pattern(int, int, int, int);
+
+int main() {
+    int n, m = 1, a, b;
+    printf("Enter number of rows :\n");
+    scanf("%d", & n);
+    a = 1, b = 1;
+    pattern(a, b, n, m);
+    a = 2, b = 2, m = 0;
+    pattern(a, b, n, m);
+
+    return 0;
+}
+void pattern(int a, int b, int n, int m) {
+    int i, j, k;
+    for (i = a; i <= n; i++) {
+        k = 1;
+        for (j = b; j <= n * 2 - 1; j++) {
+            if (m) {
+                if ((j >= n + 1 - i) && (j <= n - 1 + i) && k) {
+                    printf("*");
+                    k = 0;
+
+                } else {
+                    printf(" ");
+                    k = 1;
+                }
+
+            } else {
+                if ((j >= i + 1) && (j <= n * 2 - i + 1) && k) {
+                    printf("*");
+                    k = 0;
+                } else {
+                    printf(" ");
+                    k = 1;
+                }
+
+            }
+
+
+        }
+        printf("\n");
+    }
+
+}
